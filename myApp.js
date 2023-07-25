d93e5c8aac21c257db17ff5ae8777ba8aac0f04c
Nicole <lancasternj@gmail.com>
2023-07-25 15:40:00 +0100

    creating new endpoint and serving json as a repsonse

--- myApp.js
+++ myApp.js
@@ -7,8 +7,12 @@ const serveHtmlPath = (req, res) => {
   return res.sendFile(absolutePath);
 };
 
-app.use("/public", express.static(`${__dirname}/public`))
-app.get("/", serveHtmlPath);
+const helloJson = (req, res) => {
+  return res.send({ message: "Hello json" });
+};
 
+app.use("/public", express.static(`${__dirname}/public`));
+app.get("/", serveHtmlPath);
+app.get("/json", helloJson);
 
 module.exports = app;
