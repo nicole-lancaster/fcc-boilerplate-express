0ceecc790d83b1dc3a35d64a7c5d5985b312384f
Nicole <lancasternj@gmail.com>
2023-07-25 15:34:29 +0100

    serving static pages (CSS stylesheets)

--- myApp.js
+++ myApp.js
@@ -7,6 +7,8 @@ const serveHtmlPath = (req, res) => {
   return res.sendFile(absolutePath);
 };
 
+app.use("/public", express.static(`${__dirname}/public`))
 app.get("/", serveHtmlPath);
+
 
 module.exports = app;
